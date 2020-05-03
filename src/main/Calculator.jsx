298a967c1bf0b8e822c75230ef2a80d8ce88950b
afmdanielwin <danielfarias380@gmail.com>
2020-05-03 15:32:52 -0300

    calculator component

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Calculator.css';
+
+function Calculator(props) {
+    return (
+        <div className="calculator">
+
+        </div>
+    )
+}
+
+export default Calculator;
