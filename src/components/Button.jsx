9b81a30ce7c964177b0fd17053a4f41f3125f4ea
afmdanielwin <danielfarias380@gmail.com>
2020-05-03 17:27:34 -0300

    add event listener

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,10 @@ import './Button.css';
 
 function Button(props) {
     return (
-        <button className={`button ${props.className}`}>
+        <button 
+            className={`button ${props.className}`}
+            onClick={e => props.click(props.label)}  
+        >
             {props.label}
         </button>
     )
